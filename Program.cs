818887fe5696f450ee59642ccb946dda09703c7b
NatalyaLevinskaya <na-le4@mail.ru>
2022-08-14 14:54:18 +0300

    Added tasks

--- Program.cs
+++ Program.cs
@@ -107,3 +107,33 @@ int Max(int arg1, int arg2, int arg3)
    //     Max = (a2,b2,c2),
    //     Max = (a3,b3,c3));
 
+// Найти максимум мз 9 чисел с помощью массива (вариант 3)
+
+int Max (int arg1, int arg2, int arg3)
+{
+   int result = arg1;
+
+   if(arg2 > result) result = arg2;
+   if(arg3 > result) result = arg3;
+   return result;
+} 
+// Определим массив[int], дадим наименование (array)
+//          index   0  1  2  3  4  5  6  7  8
+   int [] array = {11,21,31,41,15,61,17,18,19};
+//  В фигурных скобках {} перечисляем значение, которое хотим использовать.  
+
+array [0] = 11; 
+// Обращаемся к конкретному элементу. Пишем его имя и индекс
+
+Console.WriteLine(array[0]);
+
+// переменная max пишется с маленькой буквы
+// функция Max пишется с большой буквы 
+   int max = Max(      
+              Max (array[0], array[1], array[2]),
+              Max (array[3], array[4], array[5]),
+              Max (array[6], array[7], array[8])
+
+   );
+   Console.WriteLine(max);
+   
